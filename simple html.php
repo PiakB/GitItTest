461ec6bec79107b17d5f52199d509b33c8699e38
Piak <aurablade@live.nl>
2018-09-11 11:37:06 +0200

    file added

---
+++ simple html.php
@@ -0,0 +1,20 @@
+<doctype? html>
+<head>
+</head>
+<body>
+<table>
+  <tr>
+    <th> test </th>
+    <th> testing </th>
+    <br>
+    <th> does this work? </th>
+  </tr>
+  <br>
+  <br>
+  </br>
+  <tr>
+    <tb> does this also work </tb>
+    <tb> does it make any difference </tb>
+  </tr>
+
+</body>
